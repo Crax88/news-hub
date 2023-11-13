02531dea1b37c923444af5cba6e4529c89dc41ca
Crax <ntrafilkin88@gmail.com>
2023-11-13 14:10:49 +0300

    Use api url

--- frontend/src/shared/api/api-instance.ts
+++ frontend/src/shared/api/api-instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosError, AxiosRequestConfig } from "axios";
 // "https://news-hub-api-a9vv.onrender.com/api";
 export const apiInstance = axios.create({
-  baseURL: "http://localhost:4000/api", //import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
