05fdf5d1c305a96a6ae4161e323f43ebcd8d99f8
Crax <ntrafilkin88@gmail.com>
2023-11-13 14:05:59 +0300

    Fix import error

--- frontend/src/widgets/navbar/navbar.tsx
+++ frontend/src/widgets/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import classes from "./Navbar.module.css";
+import classes from "./navbar.module.css";
 import { ReactNode } from "react";
 import { UiLink } from "@/shared/ui";
 import { useSessionQuery } from "@/entities/session";
